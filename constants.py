f84a9064259b38c57ffadce31f97d928e171812d
rw-a <rwa2684@gmail.com>
2023-02-10 10:05:54 +1000

    added constants.py

---
+++ constants.py
@@ -0,0 +1,23 @@
+IMAGES_DIRECTORY = {
+    2020: {
+        "Internals": 6,          # this means that the internals results are always on page 6 of the subject report
+        "Externals": (9, 10),    # this means that the externals results are in a possible range page 9-10
+        "Total": None            # total assessment marks graph doesn't exist in 2020
+    },
+    2021: {
+        "Internals": 5,
+        "Externals": 9,
+        "Total": 10              # internals + externals total (out of 100)
+    },
+    2022: {
+        "Internals": 5,
+        "Externals": 9,
+        "Total": 9
+    }
+}
+
+AXIS_INTERVALS = {
+    "Internals": 5,
+    "Externals": 5,
+    "Total": 10
+}
