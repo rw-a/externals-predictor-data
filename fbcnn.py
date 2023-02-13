116faa7d7988e61b34bde82d9c15855838a0d5ad
rw-a <rwa2684@gmail.com>
2023-02-13 10:55:23 +1000

    init

---
+++ fbcnn.py
@@ -0,0 +1,81 @@
+"""To be used in Google Colab"""
+import os.path
+import logging
+import torch
+import cv2
+from utils import utils_logger
+from utils import utils_image as util
+import requests
+
+
+def main():
+    quality_factor = 90
+    input_path = "INPUT"
+    output_path = "OUTPUT"
+    model_name = 'fbcnn_color.pth'
+    n_channels = 3            # set 1 for grayscale image, set 3 for color image
+
+    nc = [64, 128, 256, 512]
+    nb = 4
+
+    result_name = model_name[:-4]
+    util.mkdir(output_path)
+
+    model_pool = 'model_zoo'  # fixed
+    model_path = os.path.join(model_pool, model_name)
+    if os.path.exists(model_path):
+        print(f'loading model from {model_path}')
+    else:
+        os.makedirs(os.path.dirname(model_path), exist_ok=True)
+        url = 'https://github.com/jiaxi-jiang/FBCNN/releases/download/v1.0/{}'.format(os.path.basename(model_path))
+        r = requests.get(url, allow_redirects=True)
+        print(f'downloading model {model_path}')
+        open(model_path, 'wb').write(r.content)
+
+    logger_name = result_name + '_qf_' + str(quality_factor)
+    utils_logger.logger_info(logger_name, log_path=os.path.join(output_path, logger_name+'.log'))
+    logger = logging.getLogger(logger_name)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # ----------------------------------------
+    # load model
+    # ----------------------------------------
+
+    from models.network_fbcnn import FBCNN as net
+    model = net(in_nc=n_channels, out_nc=n_channels, nc=nc, nb=nb, act_mode='R')
+    model.load_state_dict(torch.load(model_path), strict=True)
+    model.eval()
+    for k, v in model.named_parameters():
+        v.requires_grad = False
+    model = model.to(device)
+    logger.info('Model path: {:s}'.format(model_path))
+
+    output_paths = util.get_image_paths(input_path)
+    for idx, img in enumerate(output_paths):
+
+        # ------------------------------------
+        # (1) img_L
+        # ------------------------------------
+        img_name, ext = os.path.splitext(os.path.basename(img))
+        logger.info('{:->4d}--> {:>10s}'.format(idx+1, img_name+ext))
+        img_L = util.imread_uint(img, n_channels=n_channels)
+
+        if n_channels == 3:
+         img_L = cv2.cvtColor(img_L, cv2.COLOR_RGB2BGR)
+        _, encimg = cv2.imencode('.jpg', img_L, [int(cv2.IMWRITE_JPEG_QUALITY), quality_factor])
+        img_L = cv2.imdecode(encimg, 0) if n_channels == 1 else cv2.imdecode(encimg, 3)
+        if n_channels == 3:
+         img_L = cv2.cvtColor(img_L, cv2.COLOR_BGR2RGB)
+        img_L = util.uint2tensor4(img_L)
+        img_L = img_L.to(device)
+
+        # ------------------------------------
+        # (2) img_E
+        # ------------------------------------
+
+        img_E,QF = model(img_L)
+        img_E = util.tensor2single(img_E)
+        img_E = util.single2uint(img_E)
+
+        util.imsave(img_E, os.path.join(output_path, img_name+'.png'))
